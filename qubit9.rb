e17317d60e266f4f67508bc5f815bfc570632a0e
Marc Jäger <59584143+Falumpaset@users.noreply.github.com>
2022-05-01 00:33:21 +0200

    Update qubit9.rb

--- qubit9.rb
+++ qubit9.rb
@@ -3,9 +3,10 @@ https://github.com/Qubit-9/q9-cli/releases/download/alpha/qubit9-0.1.1-aarch64-a
 class qubit9 < Formula
   desc "Cli ti interact with Storage9"
   homepage "https://github.com/Qubit-9/q9-cli"
-  url ".../qubit9-0.1.1-aarch64-apple-darwin.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/Qubit-9/q9-cli/releases/download/alpha/qubit9-darwin.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "a72bf90543c8a6ae65a69feab2c7cfeb7b4e1fba"
   head "https://github.com/Qubit-9/q9-cli.git"
+  version "0.1.1"
 
   def install
     bin.install "qubit9"
