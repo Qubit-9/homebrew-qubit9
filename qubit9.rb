4a8f88c6839b1be8fb8be98bd3a0f89ec2c78776
Marc Jäger <59584143+Falumpaset@users.noreply.github.com>
2022-05-01 00:58:10 +0200

    Update qubit9.rb

--- qubit9.rb
+++ qubit9.rb
@@ -1,17 +1,12 @@
 class Qubit9 < Formula
   desc "Cli ti interact with Storage9"
   homepage "https://github.com/Qubit-9/homebrew-qubit9"
-  url "https://github.com/Qubit-9/homebrew-qubit9/releases/download/alpha/qubit9-apple-darwin.tar.gz"
-  sha256 "a72bf90543c8a6ae65a69feab2c7cfeb7b4e1fba"
+  url "https://github.com/Qubit-9/homebrew-qubit9/releases/download/alpha/qubit9v0.1.1-apple-darwin.tar.gz"
+  sha256 "342c56a8a83992689cbd67ad68e8a257617a1acfb22f1b4149c64389ab4fd11d"
   head "https://github.com/Qubit-9/q9-cli.git"
   version "0.1.1"
 
   def install
     bin.install "qubit9"
-  end
-
-  # Homebrew requires tests.
-  test do
-    assert_match "qubit9 v0.1.1", shell_output("#{bin}/qubit9 --version", 2)
   end
 end
